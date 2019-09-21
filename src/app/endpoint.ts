17e700582610e2dc4e6de00f567dc0406565b5f8
אלינור ג <elinor_gargi@walla.com>
2019-09-21 23:14:43 +0300

    Initial commit

--- src/app/endpoint.ts
+++ src/app/endpoint.ts
@@ -1,9 +1,9 @@
 const apiKey: string = `m80B8Z4rQzE2w30WtuOFJj6QvZuCvO9d`;
 
-export const autocompleteUrl: string = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&language=en-us`;
+export const autocompleteUrl: string = `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&language=en-us`;
 
-export const rootCurrentWeather: string = `http://dataservice.accuweather.com/currentconditions/v1/`;
+export const rootCurrentWeather: string = `https://dataservice.accuweather.com/currentconditions/v1/`;
 export const currentWeatherParams: string = `?apikey=${apiKey}&language=en-us&details=false`;
 
-export const rootForecast5Days: string = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/`;
+export const rootForecast5Days: string = `https://dataservice.accuweather.com/forecasts/v1/daily/5day/`;
 export const forecast5DaysParams: string = `?apikey=${apiKey}&details=false&metric=true`;
